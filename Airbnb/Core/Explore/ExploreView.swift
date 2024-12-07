ea2963359609b6a9e9f8dbc7adb4ff1478b6f778
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-12-07 12:09:08 +0700

    Listing View

---
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -0,0 +1,29 @@
+//
+//  ExploreView.swift
+//  Airbnb
+//
+//  Created by 14 thang 8 on 12/7/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { listing in
+                        ListingItemView()
+                            .frame(height: 400)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
