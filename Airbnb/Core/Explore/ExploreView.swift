81c28c657af1e3a4673b1c58e4c182b339f11bc2
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-12-07 19:35:30 +0700

    Complete details view

--- Airbnb/Core/Explore/ExploreView.swift
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -25,7 +25,8 @@ struct ExploreView: View {
                     .padding()
                 }
                 .navigationDestination(for: Int.self) { listing in
-                    
+                    ListingDetailsView()
+                        .navigationBarBackButtonHidden()
                 }
             }
         }
