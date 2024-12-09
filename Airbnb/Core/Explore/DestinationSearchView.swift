6ff422875ea0074813679771b41362a2550903a7
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-12-09 16:53:34 +0700

    DestinationSearchView

---
+++ Airbnb/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  Airbnb
+//
+//  Created by 14 thang 8 on 12/8/24.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
